31a2ec38c6c00a365d8a219b0aec8617057ca18f
EduardMisa <eduardmisa@gmail.com>
2020-05-31 15:28:54 +0800

    dark

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,7 @@ module.exports = {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: false,
+      dark: true,
       themes: {
         dark: {
           primary: colors.brown.lighten3,
